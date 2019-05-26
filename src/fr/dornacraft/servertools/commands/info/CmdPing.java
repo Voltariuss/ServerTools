a697c0f6df1fb9892dd2d258f144a4216dea5046
BuildTools <unconfigured@null.spigotmc.org>
2019-05-26 13:45:02 +0200

    add config for ping command

--- src/fr/dornacraft/servertools/commands/info/CmdPing.java
+++ src/fr/dornacraft/servertools/commands/info/CmdPing.java
@@ -1,11 +1,14 @@
 package fr.dornacraft.servertools.commands.info;
 
+import java.util.HashMap;
+
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.craftbukkit.v1_12_R1.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 
+import fr.dornacraft.servertools.ServerToolsConfig;
 import fr.voltariuss.simpledevapi.MessageLevel;
 import fr.voltariuss.simpledevapi.UtilsAPI;
 import fr.voltariuss.simpledevapi.cmds.CommandArgument;
@@ -17,7 +20,7 @@ import fr.voltariuss.simpledevapi.cmds.DornacraftCommandExecutor;
 public class CmdPing extends DornacraftCommand {
 
 	public static final String CMD_LABEL = "ping";
-	private static final String CMD_DESC = "Vous indique la latence du joueur ciblé.";
+	private static final String CMD_DESC = ServerToolsConfig.getCommandMessage(CMD_LABEL, "cmd_desc");
 
 	public CmdPing() {
 		super(CMD_LABEL);
@@ -29,8 +32,13 @@ public class CmdPing extends DornacraftCommand {
 				if (args.length == 0
 						|| sender instanceof Player && args[0].equalsIgnoreCase(((Player) sender).getName())) {
 					if (sender instanceof Player) {
-						UtilsAPI.sendSystemMessage(MessageLevel.INFO, sender, "§6Pong ! §8%s §c%s ms",
-								UtilsAPI.CARAC_DOUBLE_QUOTE_END, getPing((Player) sender));
+						String ping = Integer.toString(getPing((Player) sender));
+
+						HashMap<String, String> values = new HashMap<>();
+						values.put("Ping", ping);
+
+						UtilsAPI.sendSystemMessage(MessageLevel.INFO, sender,
+								ServerToolsConfig.getCommandMessage(CMD_LABEL, "info_ping_himself", values));
 					} else {
 						UtilsAPI.sendSystemMessage(MessageLevel.INFO, sender, UtilsAPI.PLAYER_OFFLINE);
 					}
@@ -38,9 +46,14 @@ public class CmdPing extends DornacraftCommand {
 					Player target = Bukkit.getPlayer(args[0]);
 
 					if (target != null) {
+						String ping = Integer.toString(getPing(target));
+
+						HashMap<String, String> values = new HashMap<>();
+						values.put("Ping", ping);
+						values.put("Player", target.getDisplayName());
+
 						UtilsAPI.sendSystemMessage(MessageLevel.INFO, sender,
-								"Temps de réponse du joueur §b%s §e: §c%s ms", target.getDisplayName(),
-								getPing(target));
+								ServerToolsConfig.getCommandMessage(CMD_LABEL, "info_ping_target", values));
 					} else {
 						UtilsAPI.sendSystemMessage(MessageLevel.ERROR, sender, UtilsAPI.PLAYER_UNKNOW);
 					}
@@ -51,7 +64,6 @@ public class CmdPing extends DornacraftCommand {
 		getCmdTreeExecutor().getRoot().setExecutor(executor);
 		getCmdTreeExecutor().addSubCommand(
 				new CommandNode(new CommandArgument(CommandArgumentType.PLAYER, false), CMD_DESC, executor, null));
-
 	}
 
 	private int getPing(Player player) {

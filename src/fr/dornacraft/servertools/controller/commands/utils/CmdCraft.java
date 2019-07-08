182460aa47bfd327b1247ed93bb542ebe3c87c55
Voltariuss <loic.duboistermoz@gmail.com>
2019-07-08 22:58:59 +0200

    Update /craft command

--- src/fr/dornacraft/servertools/controller/commands/utils/CmdCraft.java
+++ src/fr/dornacraft/servertools/controller/commands/utils/CmdCraft.java
@@ -4,6 +4,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import fr.dornacraft.servertools.utils.ServerToolsConfig;
 import fr.voltariuss.simpledevapi.MessageLevel;
 import fr.voltariuss.simpledevapi.UtilsAPI;
 import fr.voltariuss.simpledevapi.cmds.DornacraftCommand;
@@ -18,15 +19,16 @@ public class CmdCraft extends DornacraftCommand {
 		DornacraftCommandExecutor executor = new DornacraftCommandExecutor() {
 
 			@Override
-			public void execute(CommandSender sender, Command arg1, String arg2, String[] args) throws Exception {
+			public void execute(CommandSender sender, Command cmd, String label, String[] args) throws Exception {
 				if (sender instanceof Player) {
 					((Player) sender).openWorkbench(null, true);
+					UtilsAPI.sendSystemMessage(MessageLevel.INFO, sender,
+							ServerToolsConfig.getCommandMessage(CMD_LABEL, "info_open_workbench"));
 				} else {
 					UtilsAPI.sendSystemMessage(MessageLevel.ERROR, sender, UtilsAPI.CONSOLE_NOT_ALLOWED);
 				}
 			}
 		};
-
 		getCmdTreeExecutor().getRoot().setExecutor(executor);
 	}
 }
